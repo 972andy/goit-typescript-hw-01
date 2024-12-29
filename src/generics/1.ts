46bdc3cd28f50118a4ca3a723454f96aef8d4839
Andrii Tsapko <sandriy972@gmail.com>
2024-12-29 16:18:48 +0200

    add part two of task

---
+++ src/generics/1.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+async function fetchData<T>(url: string): Promise<T> {
+  try {
+    const response: T = await axios.get(url);
+    return response;
+  } catch (error: any) {
+    throw new Error(`Error fetching from ${url}: ${error}`);
+  }
+}
---
+++ src/generics/4.ts
@@ -0,0 +1,14 @@
+type User = {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+}
+function createOrUpdateUser<T extends Partial<User>>(initialValues: T) {
+    // Оновлення користувача
+    return initialValues
+}
+createOrUpdateUser({ 
+  email: 'user@mail.com', 
+  password: 'password123' 
+});
